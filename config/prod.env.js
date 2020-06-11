aab7a0be732f471de12a76f6aa7f49057b7c9585
SmaVivian <vivi163abc@163.com>
2020-06-11 19:23:06 +0800

    本地使用本地服务，生产环境使用easy-mock线上服务

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://easy-mock.com/mock/5c3c2ce0de22053ce38a84de/vivi"',
+  BASE_API: '"https://easy-mock.com/mock/5c3c2ce0de22053ce38a84de/vivi"',
 }
